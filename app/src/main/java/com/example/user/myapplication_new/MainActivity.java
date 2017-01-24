c88051404f1e0ca9390255af14ae4a829011485f
EranSharon <eransharon10@gmail.com>
2017-01-24 17:55:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/user/myapplication_new/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.user.myapplication_new;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, PasswordsScreenActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
